edf437b4b57d653264f5587a5b9632c50fc48490
techleonic <techleonic@gmail.com>
2024-05-30 07:39:53 -0600

    multi value function

--- functions.dart
+++ functions.dart
@@ -1,9 +1,30 @@
+import 'switch.dart';
+
 void main() {
   //funtions
   printName();
+  String name1 = get_name();
+  print(name1);
+  var ageName = getNameAge();
+  print(ageName.$1); // for geting on value from a multivalue function
+  print(ageName.$2);
+
+  //another form to get multivalue functions
+  var (age, name) = getNameAge();
+  print(name);
+  print(age);
 }
 // functions
 
 void printName() {
   print("leonidas");
+}
+
+String get_name() {
+  return 'leonidas';
+}
+
+// RETURN 2 DATATYPES
+(int, String) getNameAge() {
+  return (27, "leonias");
 }
