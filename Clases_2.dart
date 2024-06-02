79c1f752f31ee14c600f36c0ff6ed96e89117990
techleonic <techleonic@gmail.com>
2024-06-02 10:48:45 -0600

    class constructor 2

--- Clases_2.dart
+++ Clases_2.dart
@@ -1,6 +1,33 @@
-void main() {}
+import 'dart:ffi';
+
+void main() {
+  final cookie = Cookie("circule", 25); //positional arguments
+  print(cookie.shape);
+  print(cookie.size);
+}
 
 class Cookie {
+  String shape;
+  double size;
   //constructor
-  Cookie() {}
+  Cookie(this.shape, this.size) {
+    //constructor
+    print("Cookie constructor call"); //it triggers every time it gets call
+    baking();
+  }
+
+  //metods
+  void baking() {
+    print("baking was started");
+  }
+
+  bool isColing() {
+    return false;
+  }
+}
+
+class Cookie2 {
+  String shape;
+  double size;
+  Cookie2(this.shape, this.size); //simple contructor
 }
