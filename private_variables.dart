56ec00c2e055b4383941a9fb6ce3f8702714731c
techleonic <techleonic@gmail.com>
2024-06-03 17:09:38 -0600

    getters & setters

--- private_variables.dart
+++ private_variables.dart
@@ -4,15 +4,37 @@ void main() {
   final cookie = Cookie(shape: "Squuare", size: 25);
   // you can access a private variable in the same file or class
   print(cookie._height);
-  print(_width);
+  print(cookie._width);
   // you can't get a private variable with another file.dart
+
+  //using getters
+  print(cookie.height);
+  print(cookie.width);
+
+  print(cookie.calculateSize());
+
+  //using setters
+  cookie.sethHeight = 12;
+  print(cookie.height);
 }
 
 class Cookie {
   final String shape;
   final double size;
   Cookie({required this.shape, required this.size});
-  int _height = 0; //private parameter
-}
+  int _height = 5; //private parameter
+  int _width = 6;
 
-int _width = 0;
+  //Geters
+  int get height => _height;
+  int get width => _width;
+
+  //Setters
+  set sethHeight(int h) {
+    _height = h;
+  }
+
+  int calculateSize() {
+    return height * width;
+  }
+}
