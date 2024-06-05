0ded36e57c756a5d8fc0f1495ce9eb4b440f9290
techleonic <techleonic@gmail.com>
2024-06-05 16:18:34 -0600

    super implement and extend

--- implemets.dart
+++ implemets.dart
@@ -1,4 +1,8 @@
-void main() {}
+void main() {
+  final towTruck = TowTruck();
+  print(towTruck.toString());
+  print(towTruck.brand);
+}
 
 class Vehicule {
   int noOfWheels = 4;
@@ -40,5 +44,25 @@ class truck implements Vehicule {
   @override
   int acelerate() {
     return speed += 80;
+  }
+}
+
+// You can extend the class give it the same properies as a super class
+// and you can implement another class to override their properies
+class TowTruck extends Vehicule implements truck {
+  String brand = "ford 150 super duty";
+
+  @override
+  int noOfWheels = 4;
+
+  @override
+  int speed = 40;
+
+  @override
+  bool isEngineRuning = true;
+
+  @override
+  int acelerate() {
+    return speed += 10;
   }
 }
