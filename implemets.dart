fa5aae98f5efed71498ab5552f3952a60b8521ad
techleonic <techleonic@gmail.com>
2024-06-05 10:17:10 -0600

    implements

---
+++ implemets.dart
@@ -0,0 +1,28 @@
+void main() {}
+
+class Vehicule {
+  int noOfWheels = 4;
+  int speed = 100;
+  bool isEngineRuning = true;
+
+  void acelerate() {
+    speed += 10;
+  }
+}
+
+//when you implements a class you have to override everything
+class bike implements Vehicule {
+  @override
+  int noOfWheels = 4;
+
+  @override
+  int speed = 100;
+
+  @override
+  bool isEngineRuning = true;
+
+  @override
+  int acelerate() {
+    return speed += 10;
+  }
+}
