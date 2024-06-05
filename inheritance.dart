746bb419d6fd55bd95cca3ae6fd5b954c241425f
techleonic <techleonic@gmail.com>
2024-06-05 09:52:02 -0600

    inheritance

--- inheritance.dart
+++ inheritance.dart
@@ -12,18 +12,37 @@ void main() {
 
   Vehicule car2 = Car();
   print((car2 as Car).noOfWheels); //treat car  as Car class
+  print((car2 as Vehicule).isLightOn);
+  dynamic someValue = 3;
+  print((someValue as int)
+      .isEven); // you can treat a dynamic variable as a integer
+
+  print(car2
+      .speed); //this comes from someClass inheritance that comes from Vehicule that extends from someClass
+  car2.accelerate(); //overrides the function
+  print(car2.speed);
 }
 
-class Vehicule {
+class someClass {
+  int speed = 10;
+  void accelerate() {
+    speed += 20;
+  }
+}
+
+class Vehicule extends someClass {
   int speed = 10;
   bool isEngineWorking = false;
   bool isLightOn = true;
+
+  @override
   void accelerate() {
     speed += 10;
   }
 }
 
 //sub class extending having everithing that the class Vehicule has
+//ading poperies and mehods from class vehicule
 class Car extends Vehicule {
   int noOfWheels = 4;
 }
