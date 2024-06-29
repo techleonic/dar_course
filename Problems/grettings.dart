fad672bdb67c77620f2febec544af60b68b67ff3
techleonic <techleonic@gmail.com>
2024-06-29 10:22:35 -0600

    problems

--- Problems/grettings.dart
+++ Problems/grettings.dart
@@ -1,8 +1,13 @@
+import 'dart:ffi';
 import 'dart:io';
 
 void main() {
   print("Enter your name:");
   String? name = stdin.readLineSync();
 
-  print("Hello, $name");
+  print("Enter your how many times:");
+  int number = int.parse(stdin.readLineSync()!);
+
+  //says hello multiply by number given by user
+  print("Hello, $name \n" * number);
 }
