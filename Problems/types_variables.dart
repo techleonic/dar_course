f5b9e3d4f214d8eb862a1596e50dbf8e76baa7bb
techleonic <techleonic@gmail.com>
2024-06-29 11:02:17 -0600

    dynamic and var

--- Problems/types_variables.dart
+++ Problems/types_variables.dart
@@ -4,17 +4,22 @@ void main() {
   print("enter a boolean");
   bool myBool = bool.parse(stdin.readLineSync()!);
 
-  print("enter a integer");
-  int myInt = int.parse(stdin.readLineSync()!);
+  //var type of variable at time of inicialization
+  //de type canot be change after the initializacion
+  print("enter a var/integer");
+  var myInt = int.parse(stdin.readLineSync()!);
 
-  print("enter a double");
-  double myDouble = double.parse(stdin.readLineSync()!);
+  //type dynamic can be change after the inicialization
+  // but will have a type in the runtime
+  print("enter a dynamic / double");
+  dynamic myDouble = double.parse(stdin.readLineSync()!);
+  myDouble = myDouble.toString();
 
   print("enter a date like 20210830");
   DateTime myDatetime = DateTime.parse(stdin.readLineSync()!);
 
   print("tis is your double $myBool\n" +
-      "this is your integer: $myInt\n" +
-      "this is your DateTime $myDatetime\n" +
-      "this is your double: $myDouble");
+      "this is your ${myInt.runtimeType}: $myInt\n" +
+      "this is your DateTime $myDatetime\n"
+          "this is your ${myDouble.runtimeType}: $myDouble");
 }
