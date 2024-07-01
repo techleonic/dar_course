da734c570f39046ab7e7e445de9ae6b934efbd41
techleonic <techleonic@gmail.com>
2024-07-01 07:05:07 -0600

    Class

--- Problems/Clases.dart
+++ Problems/Clases.dart
@@ -1,1 +1,18 @@
+import 'dart:developer';
 
+import 'functions.dart';
+
+void main() {
+  print(User().name);
+  print(User().get_all());
+}
+
+class User {
+  String name = "Leonidas";
+  int age = 27;
+  int height = 170;
+
+  String get_all() {
+    return "$name $age $height cm";
+  }
+}
