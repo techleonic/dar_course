d4305440f15495516994e4e9da3474ceb1266cc4
techleonic <techleonic@gmail.com>
2024-07-01 07:36:47 -0600

    constructor

--- Problems/Clases.dart
+++ Problems/Clases.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:developer';
 
 import 'functions.dart';
@@ -5,6 +6,18 @@ import 'functions.dart';
 void main() {
   print(User().name);
   print(User().get_all());
+
+  final newUser = User();
+  newUser.name = "Dania";
+  newUser.age = 23;
+  newUser.height = 160;
+  print(newUser.get_all());
+
+  final newAdmin = Admin("kenia", "123456");
+  newAdmin.age = 20;
+  newAdmin.name = "kenia";
+  newAdmin.height = 150;
+  print(newAdmin.get_all());
 }
 
 class User {
@@ -15,4 +28,10 @@ class User {
   String get_all() {
     return "$name $age $height cm";
   }
+}
+
+class Admin extends User {
+  String adminName;
+  String password;
+  Admin(this.adminName, this.password);
 }
