becb02da3aaf6a21e6cb43365f66115ac72a1007
techleonic <techleonic@gmail.com>
2024-07-22 18:55:48 -0600

    future

--- Problems/futures.dart
+++ Problems/futures.dart
@@ -1,3 +1,17 @@
-void main(List<String> args) {
+void main(List<String> args) async {
   //Futures
+  //Instance of 'Future<String>'
+
+  final result = await giveAResultAfter2sec();
+  print(result);
+
+  print("hey");
+  print('hello');
+  print("greeetings");
+}
+
+Future<String> giveAResultAfter2sec() {
+  return Future.delayed(Duration(seconds: 2), () async {
+    return 'hey!!';
+  });
 }
